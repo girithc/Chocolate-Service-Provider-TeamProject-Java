fb1ea97b15a79f5a5a3a922d77f535bdb1497d29
jhbischoff <jhbischoff@crimson.ua.edu>
2020-11-09 01:59:23 -0600

    Saving WeeksServices and ServicePerformed

--- src/src/chocan/ServicePerformed.java
+++ src/src/chocan/ServicePerformed.java
@@ -1,5 +1,11 @@
 package chocan;
 
+/**
+ * Class for services
+ * @author Jake Bischoff
+ *
+ */
+
 public class ServicePerformed {
 	
 	private String currDateAndTime;
@@ -9,50 +15,105 @@ public class ServicePerformed {
 	private int serviceCode;
 	private String comments;
 	
+	/**
+	 * Constructor for service
+	 */
+	public ServicePerformed() {
+		
+	}
+	
+	/**
+	 * Used to set the name
+	 * @param dateAndTime, current date and time to be input
+	 */
 	public void setCurrDateAndTime(String dateAndTime) {
 		currDateAndTime = dateAndTime;
 	}
 	
+	/**
+	 * Used to retrieve the name
+	 * @return currDateAndTime, returns the current date and time
+	 */
 	public String getCurrDateAndTime() {
 		return currDateAndTime;
 	}
 	
+	/**
+	 * Used to set the name
+	 * @param date, date the service was provided on
+	 */
 	public void setDateProvided(String date) {
 		dateProvided = date;
 	}
 	
+	/**
+	 * Used to retrieve the name
+	 * @return dateProvided, returns the date of the service
+	 */
 	public String getDateProvided() {
 		return dateProvided;
 	}
 	
+	/**
+	 * Used to set the name
+	 * @param id, the provider id assigned with the service
+	 */
 	public void setProviderNumber(int id) {
 		providerNumber = id;
 	}
 	
+	/**
+	 * Used to retrieve the name
+	 * @return providerNumber, returns the id of the provider
+	 */
 	public int getProviderNumber() {
 		return providerNumber;
 	}
 	
+	/**
+	 * Used to set the name
+	 * @param id, member id who recieved the service
+	 */
 	public void setMemberNumber(int id) {
 		memberNumber = id;
 	}
 
+	/**
+	 * Used to retrieve the name
+	 * @return memberNumber, returns the id of the member
+	 */
 	public int getMemberNumber() {
 		return memberNumber;
 	}
 	
+	/**
+	 * Used to set the name
+	 * @param code, code of the service being performed
+	 */
 	public void setServiceCode(int code) {
 		serviceCode = code;
 	}
 	
+	/**
+	 * Used to retrieve the name
+	 * @return serviceCode, returns the code of the service
+	 */
 	public int getServiceCode() {
 		return serviceCode;
 	}
 	
+	/**
+	 * Used to set the name
+	 * @param inputComments, comments added to teh service
+	 */
 	public void setComments(String inputComments) {
 		comments = inputComments;
 	}
 	
+	/**
+	 * Used to retrieve the name
+	 * @return comments, returns comments on the service 
+	 */
 	public String getComments() {
 		return comments;
 	}
