7ae3511aa68f003f186e8f970d9c8798540c5106
Jared Folden <jtfolden@crimson.ua.edu>
2020-11-11 16:30:28 -0600

    Added getMember and getProvider functions to InteractiveModeControl

--- src/src/chocan/InteractiveModeControl.java
+++ src/src/chocan/InteractiveModeControl.java
@@ -5,6 +5,20 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class InteractiveModeControl {
 	
+	public InteractiveModeControl{
+		
+	}
+	
+	public Member getMember(String id) {
+		MemberDatabase memberDatabase = new MemberDatabase();
+		return memberDatabase.getMember(id);
+	}
+	
+	public Provider getProvider(string id) {
+		ProviderDatabase providerDatabase = new ProviderDatabase();
+		return providerDatabase.getProvider(id);
+	}
+	
 	public static void addMember(Member newMember) throws Exception{
 		
 		MemberDatabase mbase =  new MemberDatabase();
