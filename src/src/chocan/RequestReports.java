11f7d32b98f0eead81ed182b3cb6b1232e58f9cc
Alisha <Alisha@10.219.40.173>
2020-11-10 21:07:09 -0600

    adding some changes

--- src/src/chocan/RequestReports.java
+++ src/src/chocan/RequestReports.java
@@ -3,33 +3,35 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 
-public class RequestReports{
-	private int id;
+public class RequestReports extends WriteReports{
+	private String id;
 	private String request;
-	public int idPrompt() {
+	WriteReports r1 = new WriteReports();
+	public String idPrompt() {
 		System.out.println("Enter the user id");
 		Scanner scanner = new Scanner (System.in);  
-		int id = scanner.nextInt();
+		String id = scanner.nextLine();
 		return id;
 	}
-	public void reportRequestPrompt(){
+	public void reportRequestPrompt() throws Exception{
 		System.out.println("Enter the type of report to be generated.");
 		Scanner scanner = new Scanner (System.in);  
 		String name = scanner.next(); 
 		if(name.equalsIgnoreCase("member"))
 		{
-			//writeMemberReport(id);
+			r1.writeMemberReport(id);
 		}
 		if(name.equalsIgnoreCase("provider"))
 		{
-			//writeProviderReport(id);
+			r1.writeProviderReport(id);
 		}
+		if(name.equalsIgnoreCase("summary"))
 		{
-			//writeSummaryReport
+			r1.writeSummaryReport();
 		}
 		if(name.equalsIgnoreCase("eft"))
 		{
-			//writeEFTReport
+			r1.writeEFTReports();
 		}
 		
 }
