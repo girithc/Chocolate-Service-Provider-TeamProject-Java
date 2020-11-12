ab4ad42074318096daa161cbd3f7028fc9c36d1d
akfike <akfike@crimson.ua.edu>
2020-11-12 14:52:11 -0600

    updating InteractiveModeTerminal to delete commented-out code

--- src/src/chocan/InteractiveModeTerminal.java
+++ src/src/chocan/InteractiveModeTerminal.java
@@ -74,12 +74,6 @@ public class InteractiveModeTerminal {
 		
 		else if (action.equalsIgnoreCase("2") && memberOrProvider.equalsIgnoreCase("1")) {
 			
-			/*Scanner mObj = new Scanner(System.in);
-			System.out.println("Enter member ID of member to be deleted: ");
-			
-			String mID = mObj.nextLine();
-			int mIDNum = Integer.parseInt(mID);*/
-			
 			InteractiveModeControl interactiveModeControl = new InteractiveModeControl();
 			
 			interactiveModeControl.deleteMember();
@@ -87,12 +81,6 @@ public class InteractiveModeTerminal {
 		}
 		
 		else if (action.equalsIgnoreCase("2") && memberOrProvider.equalsIgnoreCase("2")) {
-			
-			/*Scanner mObj = new Scanner(System.in);
-			System.out.println("Enter provider ID of provider to be deleted: ");
-			
-			String pID = mObj.nextLine();
-			int pIDNum = Integer.parseInt(pID);*/
 			
 			InteractiveModeControl interactiveModeControl = new InteractiveModeControl();
 			
@@ -102,125 +90,26 @@ public class InteractiveModeTerminal {
 		
 		else if (action.equalsIgnoreCase("3") && memberOrProvider.equalsIgnoreCase("1")) {
 			
-			/*Scanner mObj = new Scanner(System.in);
-			System.out.println("Enter member ID of member to be updated: ");*/
-			
 			String mID = "";
-			//int mIDNum = Integer.parseInt(mID);
 			
 			InteractiveModeControl interactiveModeControl = new InteractiveModeControl();
 			Member member = new Member();
 			
-			//member = interactiveModeControl.getMember(mIDNum);
 			interactiveModeControl.updateMember(mID, member);
 				
 		}
 		
 		else if (action.equalsIgnoreCase("3") && memberOrProvider.equalsIgnoreCase("2")) {
 			
-			/*Scanner mObj = new Scanner(System.in);
-			System.out.println("Enter provider ID of provider to be updated: ");
-			
-			String pID = mObj.nextLine();
-			int pIDNum = Integer.parseInt(pID);*/
-			
 			String pID = "";
 			
 			InteractiveModeControl interactiveModeControl = new InteractiveModeControl();
 			Provider provider = new Provider();
 			
-			//provider = interactiveModeControl.getProvider(pIDNum);
 			interactiveModeControl.updateProvider(pID, provider);
 				
-		}
-		
-		
-		
+		}		
 		
 	}
-	
-
-	/*public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Select an action to be performed");
-		System.out.println("Enter:");
-		System.out.println("1 for Add Record");
-		System.out.println("2 for Delete Record");
-		System.out.println("3 for Update Record");
-		
-		InputStreamReader sr = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(sr);
-
-		int cType = 0; // Add / Edit / Delete
-		int cRecord = 0; // Member / Provider
-		try {
-			String line = br.readLine();
-			cType = Integer.parseInt(line);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Something went wrong.");
-		}
-		
-		//System.out.println("Thank you " + choice);
-
-		
-		if(cType > 0 && cType < 4) {
-			
-			System.out.println("Select record type");
-			System.out.println("Enter:");
-			System.out.println("1 for Member");
-			System.out.println("2 for Provider");
-			
-			try {
-				String line = br.readLine();
-				cRecord = Integer.parseInt(line);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Something went wrong!!.");
-			}
-			
-			
-			
-			if(cRecord == 1){ // Member
-				if(cType == 1) {
-					Member newMember = new Member();
-					InteractiveModeControl.addMember(newMember);
-				}
-				else if(cType == 2) {
-					
-					InteractiveModeControl.deleteMember();
-				}
-				else {
-					Member temp;
-					String Id;
-					InteractiveModeControl.updateMember(Id, temp);
-				}
-				
-			}
-			else if(cRecord == 2) {
-				if(cType == 1) {
-					Provider newProvider = new Provider();
-					InteractiveModeControl.addProvider(newProvider);
-				}
-				else if(cType == 2) {
-					
-					InteractiveModeControl.deleteProvider();
-				}
-				else {
-					Provider temp;
-					String Id;
-					InteractiveModeControl.updateProvider(Id, temp);
-				}
-			}
-			
-			
-			
-		}
-
-		
-		
-
-	}*/
 
 }
