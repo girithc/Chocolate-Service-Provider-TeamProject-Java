06cd1bd2b3403fb510d7761da397274c42e20e7a
jhbischoff <jhbischoff@crimson.ua.edu>
2020-12-01 20:32:31 -0500

    Finished Member/Provider tester file

--- src/src/chocan/MemberandProviderTest.java
+++ src/src/chocan/MemberandProviderTest.java
@@ -7,54 +7,38 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MemberandProviderTest {
-
-	WriteReports report;
+	
+	MemberDatabase members;
+	ProviderDatabase providers;
 	
 	@Before
 	public void setUp() throws Exception {
-		report = new WriteReports();
+		members = new MemberDatabase();
+		providers = new ProviderDatabase();
 	}
-
+	
 	@Test
-	public void test() throws Exception {
-		
-		report.writeProviderReport("0");							//Call writeProviderReport. Test with a predetermined entry in provider database
-		report.writeMemberReport("0");								//Call writeMemberReport. Test with a predetermined entry in member database
-		
-		
-		File providerReport = new File("Provider.txt");				//Open the provider file written by writeProviderReport
-		Scanner scanProvider = new Scanner(providerReport);
-		String currentLine;
-		currentLine = scanProvider.nextLine();
-		assertEquals("Provider name: Sam Noyes", currentLine);		//Scan each line of the file to match the test input
-		currentLine = scanProvider.nextLine();
-		assertEquals("Provider number: 11547298", currentLine);
-		currentLine = scanProvider.nextLine();
-		assertEquals("Provider street address: 608 6th Avenue", currentLine);
-		currentLine = scanProvider.nextLine();
-		assertEquals("Provider state: AL", currentLine);
-		currentLine = scanProvider.nextLine();
-		assertEquals("Provider ZIP code: 35404", currentLine);
-		
-		scanProvider.close();			//Close the scanner
-		
-		File memberReport = new File("Member.txt");				//Open the provider file written by writeMemberReport
-		Scanner scanMember = new Scanner(memberReport);
-		
-		currentLine = scanMember.nextLine();
-		assertEquals("Member name: Sam Noyes", currentLine);		//Scan each line of the file to match the test input
-		currentLine = scanMember.nextLine();
-		assertEquals("Member number: 11547298", currentLine);
-		currentLine = scanMember.nextLine();
-		assertEquals("Member street address: 608 6th Avenue", currentLine);
-		currentLine = scanMember.nextLine();
-		assertEquals("Member state: AL", currentLine);
-		currentLine = scanMember.nextLine();
-		assertEquals("Member ZIP code: 35404", currentLine);
-		
-		scanMember.close();			//Close the scanner
-		
-		fail("Not yet implemented");
+	public void providerNameSuccessTest() throws Exception {
+		String name = providers.getProvider("11880129").getName();
+		assertEquals("Jared Folden", name);
+	}
+	
+	@Test
+	public void memberNameSuccessTest() throws Exception {
+		String name = members.getMember("11926699").getName();
+		assertEquals("Anne Fike", name);
+	}
+	
+	@Test
+	public void providerNameFailureTest() throws Exception {
+		String name = providers.getProvider("a").getName();
+		assertEquals("Provider not found", name);
+	}
+	
+	@Test
+	public void memberNameFailureTest() throws Exception {
+		String name = members.getMember("a").getName();
+		assertEquals("Member not found", name);
 	}
 
 }
